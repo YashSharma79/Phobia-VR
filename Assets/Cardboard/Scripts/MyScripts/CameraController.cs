3158d757a75538b3718439ff796c68397a6f3c11
Yash Sharma <yashsharma2595@gmail.com>
2020-11-14 20:23:20 +0530

    Full project now source controlled

---
+++ Assets/Cardboard/Scripts/MyScripts/CameraController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+	private Vector3 offset;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	void LateUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (moveHorizontal,0f,moveVertical);
+		transform.position += movement;
+	}
+}
---
+++ Assets/Cardboard/Scripts/MyScripts/Look.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class Look : MonoBehaviour {
+
+	public Renderer skybox;
+	public Material sky1;
+	public Material sky2;
+	public Material sky3;
+	public Material sky4;
+	private int currentSky = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	public void stare()
+	{
+		switch (currentSky) 
+		{
+		case 0:
+			skybox.material = sky2;
+			currentSky++;
+			break;
+
+		case 1:
+			skybox.material = sky3;
+			currentSky++;
+			break;
+
+		case 2:
+			skybox.material = sky3;
+			currentSky++;
+			break;
+
+		case 3:
+			skybox.material = sky4;
+			currentSky++;
+			break;
+
+		}
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
